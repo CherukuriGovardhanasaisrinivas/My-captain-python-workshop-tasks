6b677c45dfdad70a18c4a958906485f745b5b22c
CherukuriGovardhanasaisrinivas <58883123+CherukuriGovardhanasaisrinivas@users.noreply.github.com>
2020-05-12 20:25:45 +0530

    Update Task2.py
    This programme accepts a file name and prints the extension of it

--- Task2.py
+++ Task2.py
@@ -1,5 +1,3 @@
-fn= input("Enter Filename: ")
-
-f = fn.split(".")
-
-print ("Extension of the file is : " + f[-1])
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
