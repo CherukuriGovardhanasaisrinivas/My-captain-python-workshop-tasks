a325617ea1b4cafd74e92fd2289ea98ecf844b97
CherukuriGovardhanasaisrinivas <58883123+CherukuriGovardhanasaisrinivas@users.noreply.github.com>
2020-05-21 14:48:15 +0530

    Update Task-4.py

--- Task-4.py
+++ Task-4.py
@@ -2,7 +2,8 @@ list1 = [12,-7,5,64,-14]
 list2=[12,14,-95,3]
 for num in list1: 
     if num >= 0: 
-       print(num, end = " ") 
+       print(num,end=" ")
+    print()
 for num in list2:
   if num >= 0: 
-       print(num, end = " ") 
+       print(num,end=" ") 
